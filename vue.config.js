799c4ba505b8f879a5977704a015120f890699a0
lyuhongzhi <50257342+WowLv@users.noreply.github.com>
2020-10-27 14:43:13 +0800

    压缩打包配置

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
+const CompressionPlugin = require('compression-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer')
   .BundleAnalyzerPlugin
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : './',
+  productionSourceMap: process.env.NODE_ENV === 'dev',
   devServer: {
     proxy: {
       '/api': {
@@ -17,6 +19,16 @@ module.exports = {
   configureWebpack: (config) => {
     if (process.env.NODE_ENV === 'production') {
       config.plugins.push(new BundleAnalyzerPlugin())
+      config.plugins.push(
+        new CompressionPlugin({
+          algorithm: 'gzip', // 使用gzip压缩
+          test: /\.js$|\.html$|\.css$/, // 匹配文件名
+          filename: '[path].gz[query]', // 压缩后的文件名(保持原文件名，后缀加.gz)
+          threshold: 10240,
+          // deleteOriginalAssets:true, //删除源文件，不建议
+          minRatio: 1
+        })
+      )
     }
   }
 }
